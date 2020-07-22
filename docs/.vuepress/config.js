85b41f3e21e2f5f65debc405ab76ac00b09392c8
wxsm <wxsms@foxmail.com>
2020-07-22 18:51:33 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/website/',
   themeConfig: {
     logo: 'https://user-images.githubusercontent.com/5960988/87894627-d6af0480-ca75-11ea-831f-f24d2719c622.png',
     lastUpdated: 'Last Updated',
